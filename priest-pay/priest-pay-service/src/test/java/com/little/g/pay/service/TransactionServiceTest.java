bf8be63bc85948e157c294d08fefe132f7babea2
llg.java <llg68686868@163.com>
2019-04-15 21:27:57 +0800

    用户账户核心代码初始化，测试通过

---
+++ priest-pay/priest-pay-service/src/test/java/com/little/g/pay/service/TransactionServiceTest.java
@@ -0,0 +1,28 @@
+package com.little.g.pay.service;
+
+import com.little.g.demo.BaseTest;
+import com.little.g.pay.api.TransactionService;
+import com.little.g.pay.dto.NormalUserAccount;
+import com.little.g.pay.dto.TransactionRecordDTO;
+import com.little.g.pay.enums.BusinessType;
+import com.little.g.pay.enums.FixAccount;
+import com.little.g.pay.utils.TransactionNumUtil;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by lengligang on 2019/4/15.
+ */
+public class TransactionServiceTest extends BaseTest {
+
+    @Resource
+    private TransactionService transactionService;
+
+    @Test
+    public void testAdd(){
+        List<TransactionRecordDTO> list = transactionService.transfer(FixAccount.LITTLE_G.getAccount(),new NormalUserAccount(10007L),1000l, TransactionNumUtil.generateChageNum(), BusinessType.RECHARGE,"this is a test");
+        System.out.print(list);
+    }
+}
